1cbb5ee0e1097d86ac26fc57a6b5075fd2ae6531
Bernardo <znayder010896@gmail.com>
2025-02-11 19:33:07 -0600

    verifica ingreso de datos

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let Amigos = [];
+function agregarAmigo(){
+     
+     let amigo = document.getElementById('amigo').value;
+     if (isNaN(amigo)==true || amigo == '' || amigo == ' '){//VERIFICA SI NO ES UN NUMERO, ESTA VACIO O ES UN ESPACIO
+        Amigos.push(amigo);
+     }else{
+        alert('Ingrese un nombre valido');
+     };
+     console.log(Amigos.length);
+     console.log(Amigos);
+     limpiarCaja();
+}
+function limpiarCaja(){
+ let limpiar = document.querySelector('#amigo');
+ limpiar.value = ''; //LIMPIA LA CAJA
+}
