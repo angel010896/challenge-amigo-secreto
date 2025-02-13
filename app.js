8ba3c7e35dc813ce366741047cd4bb038b5d1f3f
Bernardo <znayder010896@gmail.com>
2025-02-13 13:24:06 -0600

    solucionando bucle

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let Amigos = [];
 function agregarAmigo(){
+    
      let amigo = document.getElementById('amigo').value;//SELECCIONA EL VALOR DE LA CAJA DE TEXTO Y LO GUARDA EN AMIGO
      if (amigo !== '' && !/\d/.test(amigo)){//VERIFICA SI NO ES UN NUMERO, ESTA VACIO O ES UN ESPACIO \D BUSCA NUMERO EN LA CADENA .TEST EVALUA A AMIGO ! NEGACION
         Amigos.push(amigo);
@@ -23,7 +24,7 @@ function mostrarAmigos(){
         mostrar.innerHTML += `<li>${Amigos[i]}</li>`; //AGREGA EN FORMA DE LISTA CADA UNO DE LOS AMIGOS <LI> ES PARA HACERLO EN FORMA DE LISTA
     }
 };
-function sortearAmigo(boton){
+function sortearAmigo(){
     if (Amigos.length == 0 || Amigos.length == 1){//VERIFICA QUE EL NUMERO DE AMIGOS NO SEA 1 O 0
         alert(`No se puede realizar el sorteo por que hay ${Amigos.length} ${Amigos.length == 1 ? 'amigo' : 'amigos'}`);//ENVIA ALERTA QUE NO SE PUEDE HACER EL SORTEO
     }else{
@@ -32,7 +33,7 @@ function sortearAmigo(boton){
         limpiarLista();
         let ganadorSorteo = document.getElementById('resultado');//SELECCIONA RESULTADO
         ganadorSorteo.innerHTML = `<h2> El amigo sorteado es : ${numeroElegido} </h2>`;//MUESTRA AL GANADOR
-        boton.disabled = true;
+        Amigos = [];//LIMPIA EL ARREGLO
     };
 };
 function limpiarLista(){
