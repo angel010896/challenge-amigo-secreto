738f079a592214e1cd247473038aa9fcda3222a5
Bernardo <znayder010896@gmail.com>
2025-02-13 12:57:54 -0600

    disabled en boton sorteo

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let Amigos = [];
 function agregarAmigo(){
-     
      let amigo = document.getElementById('amigo').value;//SELECCIONA EL VALOR DE LA CAJA DE TEXTO Y LO GUARDA EN AMIGO
      if (amigo !== '' && !/\d/.test(amigo)){//VERIFICA SI NO ES UN NUMERO, ESTA VACIO O ES UN ESPACIO \D BUSCA NUMERO EN LA CADENA .TEST EVALUA A AMIGO ! NEGACION
         Amigos.push(amigo);
@@ -24,7 +23,7 @@ function mostrarAmigos(){
         mostrar.innerHTML += `<li>${Amigos[i]}</li>`; //AGREGA EN FORMA DE LISTA CADA UNO DE LOS AMIGOS <LI> ES PARA HACERLO EN FORMA DE LISTA
     }
 };
-function sortearAmigo(){
+function sortearAmigo(boton){
     if (Amigos.length == 0 || Amigos.length == 1){//VERIFICA QUE EL NUMERO DE AMIGOS NO SEA 1 O 0
         alert(`No se puede realizar el sorteo por que hay ${Amigos.length} ${Amigos.length == 1 ? 'amigo' : 'amigos'}`);//ENVIA ALERTA QUE NO SE PUEDE HACER EL SORTEO
     }else{
@@ -32,10 +31,15 @@ function sortearAmigo(){
         let numeroElegido = Amigos[numeroSorteado];//SELECCIONA EL AMIGO QUE TIENE EL SUMERO ALEATORIO
         limpiarLista();
         let ganadorSorteo = document.getElementById('resultado');//SELECCIONA RESULTADO
-        ganadorSorteo.innerHTML = `<h2> ${numeroElegido} </h2>`;//MUESTRA AL GANADOR
+        ganadorSorteo.innerHTML = `<h2> El amigo sorteado es : ${numeroElegido} </h2>`;//MUESTRA AL GANADOR
+        boton.disabled = true;
     };
 };
 function limpiarLista(){
     let limpieza = document.getElementById('listaAmigos');//SELECCIONA A LISTA AMIGOS
     limpieza.innerHTML = '';//LIMPIA LA LISTA
+};
+function reiniciarJuego(){
+   location.reload();
+    
 };
